a37be64094f3628d48d447e4853c2bb691da71a3
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-07-12 21:03:22 +0530

    the tutorial file

---
+++ components/PhoneSvg.js
@@ -0,0 +1,301 @@
+const PhoneSvg = ({ skeleton, bolt, bolt1, bolt2, bolt3, bolt4, bolt5 }) => {
+  return (
+    <div>
+      <svg
+        width="397"
+        height="631"
+        viewBox="0 0 397 631"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="phone">
+          <rect width="397" height="631" fill="white" />
+          <g id="Phone" filter="url(#filter0_d)">
+            <rect
+              x="55"
+              y="62"
+              width="299"
+              height="507"
+              rx="35"
+              fill="#F7F7F7"
+            />
+          </g>
+          <g id="Skeleton" className={skeleton}>
+            <rect
+              id="Rectangle 8"
+              x="128"
+              y="257"
+              width="154"
+              height="39"
+              rx="19.5"
+              fill="#E7E6E6"
+            />
+            <circle id="Ellipse 1" cx="102" cy="107" r="14" fill="#E7E6E6" />
+            <line
+              id="Line 1"
+              x1="288"
+              y1="97"
+              x2="315"
+              y2="97"
+              stroke="#E7E6E6"
+              strokeWidth="4"
+            />
+            <line
+              id="Line 2"
+              x1="288"
+              y1="105"
+              x2="315"
+              y2="105"
+              stroke="#E7E6E6"
+              strokeWidth="4"
+            />
+            <line
+              id="Line 3"
+              x1="288"
+              y1="113"
+              x2="315"
+              y2="113"
+              stroke="#E7E6E6"
+              strokeWidth="4"
+            />
+            <rect
+              id="Rectangle 2"
+              x="88"
+              y="160"
+              width="84"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <rect
+              id="Rectangle 3"
+              x="184"
+              y="160"
+              width="71"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <rect
+              id="Rectangle 4"
+              x="265"
+              y="160"
+              width="50"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <rect
+              id="Rectangle 5"
+              x="88"
+              y="194"
+              width="128"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <rect
+              id="Rectangle 6"
+              x="222"
+              y="194"
+              width="68"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <rect
+              id="Rectangle 7"
+              x="296"
+              y="194"
+              width="19"
+              height="24"
+              rx="5"
+              fill="#E7E6E6"
+            />
+            <g id="Group 1">
+              <rect
+                id="Rectangle 9"
+                x="102"
+                y="347"
+                width="90"
+                height="84"
+                rx="5"
+                fill="#E7E6E6"
+              />
+              <path
+                id="Rectangle 10"
+                d="M102 398.732C102 397.312 102.604 395.958 103.662 395.01L126.047 374.936C127.919 373.257 130.747 373.23 132.651 374.873L153.916 393.223C156.012 395.031 159.178 394.793 160.979 392.691L169.367 382.905C171.2 380.766 174.437 380.563 176.523 382.456L190.36 395.012C191.404 395.96 192 397.304 192 398.715V426C192 428.761 189.761 431 187 431H107C104.239 431 102 428.761 102 426V398.732Z"
+                fill="#C1C0C0"
+              />
+            </g>
+            <g id="Group 2">
+              <rect
+                id="Rectangle 9_2"
+                x="212"
+                y="347"
+                width="90"
+                height="84"
+                rx="5"
+                fill="#E7E6E6"
+              />
+              <path
+                id="Rectangle 10_2"
+                d="M212 398.732C212 397.312 212.604 395.958 213.662 395.01L236.047 374.936C237.919 373.257 240.747 373.23 242.651 374.873L263.916 393.223C266.012 395.031 269.178 394.793 270.979 392.691L279.367 382.905C281.2 380.766 284.437 380.563 286.523 382.456L300.36 395.012C301.404 395.96 302 397.304 302 398.715V426C302 428.761 299.761 431 297 431H217C214.239 431 212 428.761 212 426V398.732Z"
+                fill="#C1C0C0"
+              />
+            </g>
+            <g id="Group 3">
+              <rect
+                id="Rectangle 9_3"
+                x="102"
+                y="460"
+                width="90"
+                height="84"
+                rx="5"
+                fill="#E7E6E6"
+              />
+              <path
+                id="Rectangle 10_3"
+                d="M102 511.732C102 510.312 102.604 508.958 103.662 508.01L126.047 487.936C127.919 486.257 130.747 486.23 132.651 487.873L153.916 506.223C156.012 508.031 159.178 507.793 160.979 505.691L169.367 495.905C171.2 493.766 174.437 493.563 176.523 495.456L190.36 508.012C191.404 508.96 192 510.304 192 511.715V539C192 541.761 189.761 544 187 544H107C104.239 544 102 541.761 102 539V511.732Z"
+                fill="#C1C0C0"
+              />
+            </g>
+            <g id="Group 4">
+              <rect
+                id="Rectangle 9_4"
+                x="212"
+                y="460"
+                width="90"
+                height="84"
+                rx="5"
+                fill="#E7E6E6"
+              />
+              <path
+                id="Rectangle 10_4"
+                d="M212 511.732C212 510.312 212.604 508.958 213.662 508.01L236.047 487.936C237.919 486.257 240.747 486.23 242.651 487.873L263.916 506.223C266.012 508.031 269.178 507.793 270.979 505.691L279.367 495.905C281.2 493.766 284.437 493.563 286.523 495.456L300.36 508.012C301.404 508.96 302 510.304 302 511.715V539C302 541.761 299.761 544 297 544H217C214.239 544 212 541.761 212 539V511.732Z"
+                fill="#C1C0C0"
+              />
+            </g>
+          </g>
+          <g id="bolt" className={bolt}>
+            <g id="bolt1" className={bolt1}>
+              <rect
+                id="Rectangle 9_5"
+                x="128"
+                y="257"
+                width="154"
+                height="39"
+                rx="19.5"
+                fill="#69FD44"
+              />
+              <path
+                id="Rectangle 11"
+                d="M210.627 264.015L203.299 264.019L200.149 276.483L203.289 276.482L198.047 287.484L212.713 273.976L206.432 273.98L210.627 264.015Z"
+                fill="white"
+              />
+            </g>
+            <g id="bolt2" className={bolt2}>
+              <circle
+                id="Ellipse 2"
+                cx="191.5"
+                cy="347.5"
+                r="18.5"
+                fill="#69FD44"
+              />
+              <path
+                id="Rectangle 12"
+                d="M197.627 335.015L190.299 335.019L187.149 347.483L190.289 347.482L185.047 358.484L199.713 344.976L193.432 344.98L197.627 335.015Z"
+                fill="white"
+              />
+            </g>
+            <g id="bolt3" className={bolt3}>
+              <circle
+                id="Ellipse 2_2"
+                cx="301.5"
+                cy="347.5"
+                r="18.5"
+                fill="#69FD44"
+              />
+              <path
+                id="Rectangle 12_2"
+                d="M307.627 335.015L300.299 335.019L297.149 347.483L300.289 347.482L295.047 358.484L309.713 344.976L303.432 344.98L307.627 335.015Z"
+                fill="white"
+              />
+            </g>
+            <g id="bolt4" className={bolt4}>
+              <circle
+                id="Ellipse 2_3"
+                cx="191.5"
+                cy="459.5"
+                r="18.5"
+                fill="#69FD44"
+              />
+              <path
+                id="Rectangle 12_3"
+                d="M197.627 447.015L190.299 447.019L187.149 459.483L190.289 459.482L185.047 470.484L199.713 456.976L193.432 456.98L197.627 447.015Z"
+                fill="white"
+              />
+            </g>
+            <g id="bolt5" className={bolt5}>
+              <circle
+                id="Ellipse 2_4"
+                cx="301.5"
+                cy="460.5"
+                r="18.5"
+                fill="#69FD44"
+              />
+              <path
+                id="Rectangle 12_4"
+                d="M307.627 448.015L300.299 448.019L297.149 460.483L300.289 460.482L295.047 471.484L309.713 457.976L303.432 457.98L307.627 448.015Z"
+                fill="white"
+              />
+            </g>
+          </g>
+        </g>
+        <defs>
+          <filter
+            id="filter0_d"
+            x="42"
+            y="56"
+            width="319"
+            height="527"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            />
+            <feMorphology
+              radius="2"
+              operator="erode"
+              in="SourceAlpha"
+              result="effect1_dropShadow"
+            />
+            <feOffset dx="-3" dy="4" />
+            <feGaussianBlur stdDeviation="6" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+export default PhoneSvg;
